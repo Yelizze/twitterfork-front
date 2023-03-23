9e84310b721a6db8fa2e159876560c6abad1b4a6
blobby-bobby <bulle.ouvrard@gmail.com>
2023-03-23 15:20:54 +0100

    reducers and local storage set

---
+++ reducers/tweets.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: [],
+};
+
+export const tweetSlice = createSlice({
+  // nom état
+  name: "tweets", // état initial
+  initialState,
+  reducers: {
+    newTweet: (state, action) => {
+      let tweet = {
+        user: action.payload.user,
+        message: action.payload.message,
+        isLiked: action.payload.isLiked
+      }
+      state.value.push(tweet)
+    },
+    deleteTweet: (state, action) => {
+      state.value = state.value.filter(item => item.message !== action.payload.message);
+    },
+  }
+});
+
+// Pas le même actions que dans l'init du reducer (un "s" en plus)
+export const { newTweet, deleteTweet } = tweetSlice.actions;
+export default tweetSlice.reducer;
